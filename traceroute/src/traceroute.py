8fe4ea1e59a44d9567d42238fe78c4e978aa810a
DarkAEther <30438425+DarkAEther@users.noreply.github.com>
2020-10-19 15:01:43 +0530

    feat(networking): add traceroute microbyte
    This introduces the traceroute MicroByte

---
+++ traceroute/src/traceroute.py
@@ -0,0 +1,24 @@
+from scapy.all import *
+import sys
+
+host=sys.argv[1]
+ttl = 1
+print("Tracing route to "+host);
+
+while ttl < 200:
+    ipLayer = IP()
+    ipLayer.dst = host #set destination host
+    ipLayer.ttl = ttl #set TTL for packet
+    ICMPpkt = ICMP()
+    pkt = ipLayer/ICMPpkt
+    reply = sr1(pkt, verbose = 0) #send packet and wait for reply
+    
+    if (reply is None):
+        break
+    elif (reply[ICMP].type == 0):
+        print(reply[IP].src+" is "+str(ttl)+" hops away")
+        print("Done")
+        break
+    elif (reply[ICMP].code == 0 and reply[ICMP].type == 11):
+        print(reply[IP].src+" is "+str(ttl)+" hops away")
+        ttl+=1
